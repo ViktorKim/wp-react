a3f1cac9617aa6c1d0d963c8748b51160b265840
viktorkim <fake2spam@gmail.com>
2018-10-05 13:28:09 +0300

    Created scratch WP theme(style.css, index.php, functions.php). Added React to the theme

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viktor.Kim
+ * Date: 10/5/2018
+ * Time: 12:34 PM
+ */
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viktor.Kim
+ * Date: 10/5/2018
+ * Time: 12:04 PM
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+</head>
+<footer>
+
+</footer>
+<?php wp_footer(); ?>
+</html>
